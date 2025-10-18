fad94eba56128ec628b0b012a63f9c523e49059f
GovindxSharma <govindsharma2839@gmail.com>
2025-10-18 10:05:53 +0530

    scroll top fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,22 @@ import Homepage from "./pages/Homepage";
 import ProductsPage from "./pages/ProductsPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
 import Footer from "./components/layout/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return (
     <Router>
+      {/* Scrolls to top on every route change */}
+      <ScrollToTop /> 
+
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/products/:id" element={<ProductDetailsPage />} />
       </Routes>
+
       <Footer />
     </Router>
   );
